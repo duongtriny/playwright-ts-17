d3475a99aa995531b717f561e55c3a506b037376
Duong Trinh <duong.triny@gmail.com>
2026-09-26 20:13:18 +0700

    [ID-06] Verify datetime

--- tests/web-elements/elements/datetime.spec.ts
+++ tests/web-elements/elements/datetime.spec.ts
@@ -36,3 +36,44 @@ async function selectTimePickerByLabelForNow(label: string, page: Page) {
     let nowButtonXpath = "//li[contains(concat(' ',normalize-space(@class),' '),' ant-picker-now ')]//a[normalize-space()='Now']";
     await page.locator(nowButtonXpath).click();
 }
+
+test(`Verify date`, async ({ page }) => {
+    await page.goto('https://test-with-me-app.vercel.app/learning/web-elements/elements/date-time');
+    await selectDatePickerByLabel('Date Picker', '9', 'May', '2031', page);
+    await expect(page.getByText('Current date: 2031-05-09')).toBeVisible();
+});
+
+async function selectDatePickerByLabel(label: string, dd: string, mm: string, yyyy: string, page: Page) {
+    let datePickerXpath = `//label[normalize-space()='${label}']//following::div[contains(concat(' ', @class, ' '), ' ant-picker ')][1]`;
+    await page.locator(datePickerXpath).click();
+    //Select year
+    let buttonChooseYearXpath = `//button[@aria-label='Choose a year']`;
+    await page.locator(buttonChooseYearXpath).click();
+    let buttonChooseDecadeXpath = `//button[@aria-label='Choose a decade']`;
+    let decadeText = await page.locator(buttonChooseDecadeXpath).textContent();
+    let [minYearText, maxYearText] = decadeText?.split('-') || [];
+    let minYear = Number.parseInt(minYearText);
+    let maxYear = Number.parseInt(maxYearText);
+    let year = Number.parseInt(yyyy);
+    let distance;
+    let decadeMoveButtonXpath;
+    if (year >= maxYear) {
+        decadeMoveButtonXpath = `//button[@aria-label='Next year']`;
+        distance = year - maxYear;
+    } else {
+        decadeMoveButtonXpath = `//button[@aria-label='Last year']`
+        distance = minYear - year;
+    }
+    let clickCounts = Math.ceil(distance / (maxYear - minYear + 1));
+    for (let i = 0; i < clickCounts; i++) {
+        await page.locator(decadeMoveButtonXpath).click();
+    }
+    let yearXpath = `//td[contains(concat(' ', @class, ' '), ' ant-picker-cell-in-view ') and normalize-space()='${yyyy}']`;
+    await page.locator(yearXpath).click();
+    //Select month
+    let monthXpath = `//td[contains(concat(' ', @class, ' '), ' ant-picker-cell-in-view ') and normalize-space()='${mm}']`;
+    await page.locator(monthXpath).click();
+    //Select day
+    let dayXpath = `//td[contains(concat(' ', @class, ' '), ' ant-picker-cell-in-view ') and normalize-space()='${dd}']`;
+    await page.locator(dayXpath).click();
+}
